dc660244ad53e163dc912207e4ad10ffaffdd4ef
cristina-rossi <90429880+cristina-rossi@users.noreply.github.com>
2024-08-20 23:15:00 -0400

    Update index.js

--- web/index.js
+++ web/index.js
@@ -68,7 +68,7 @@ function populateTabPage(name){
     pageContainer.className = 'pagecontainer';
     pageContainer.id = name+'_tabID';
     document.body.appendChild(pageContainer);
-    const folderPath='web/'+name;
+    const folderPath='movementvrweb/web/'+name;
     const htmlFileName=folderPath+'/'+name+'.html'; 
     const jsonPath=folderPath+'/'+name+'.json';  
     const generalJsonPath='web/general/general.json'; 
